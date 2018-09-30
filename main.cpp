b5139b4596ae58d6fa867033d78d917530189da9
Nik <x-sis@yandex.ru>
2018-09-30 14:40:41 +0300

    'v4'

--- main.cpp
+++ main.cpp
@@ -6,10 +6,12 @@ using namespace std;
 int main ()
 
 {
-    int a, b;
+    int a, b, c;
     cout << "Enter two integer numbers" << endl;
     cin >> a >> b;
     cout << "Thats OK !" << endl;
     // difference
+    c = a-b;
+    cout << "Result = " << c << endl;
     return 0;
 }
