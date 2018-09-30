6e8ec4e2c0151b69b5060cd04dd6bf8ef7414917
Nik <x-sis@yandex.ru>
2018-09-30 13:30:21 +0300

    'v1'

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+    int a, b;
+    cout << "Enter two integer numbers" << endl;
+    cin >> a >> b;
+    return 0;
+}
