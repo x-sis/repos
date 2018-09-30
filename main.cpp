f13a5db2262b08ffd6a2a0152bf16977633cd122
Nik <x-sis@yandex.ru>
2018-09-30 15:39:39 +0300

    'v5.0.1'

--- main.cpp
+++ main.cpp
@@ -11,6 +11,6 @@ int main()
     cin  >> a >> b;
     cout << "A + B = " << a + b << '\n'
     << "A - B = " << a - b << '\n'
-    << "A * B = " << a * b << '\n';
+    << "A / B = " << a / b << '\n';
 return 0;
 }
